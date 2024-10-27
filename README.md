4e27bd3317e228e4394760663302f27611622081
Yannik Hoffmann <117282045+yhoffmann@users.noreply.github.com>
2024-10-27 21:34:53 +0100

    await() now works without timer in loop. Made more robust against wrong usage. Definitions now fully in .hpp file. Updated README.
    Signed-off-by: Yannik Hoffmann <117282045+yhoffmann@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
+WIP, things might change
+
 This is based on the implementation in https://stackoverflow.com/questions/15752659/thread-pooling-in-c11 by https://stackoverflow.com/users/3818417/phd-ap-ece.
 # ThreadPool class
-Create <code>ThreadPool</code> object with
+Create `ThreadPool` object with
 ```
 ThreadPool pool;
 ```
@@ -15,7 +17,7 @@ ThreadPool pool;
 pool.start(10);
 pool.start(10);
 ```
-will start 10 threads the first time and 6 the second time <code>start(...)</code> is called. The threads created by multiple instances of the <code>ThreadPool</code> object, however, can exceed the number of logical hardware processors.
+will start 10 threads the first time and 6 the second time `::start(...)` is called. The threads created by multiple instances of the `ThreadPool` class, however, can exceed the number of logical hardware processors.
 
 ```
 ThreadPool pool; pool.start(10);
@@ -28,18 +30,32 @@ Jobs can be queued with
 ```
 pool.enq_job(f);
 ```
-where <code>f</code> is a lambda <code>\[\](void) -> void {}</code> (or <code>std::function\<void(void)\></code>). You can queue as many jobs as you like and they will be started whenever a thread is free.
-
-## Stop
-You prevent new jobs from the queue from being started with:
-```
-pool.stop();
-```
-Any runnings jobs will still be finished regularly.  
+where `f` is a lambda `[](void) -> void {}` (or `std::function<void(void)>`). You can queue as many jobs as you like and they will be started one by one whenever a thread is free.
 
 ## Await
-Of course, usually, you want all jobs to be executed. Use this:
+All jobs that were queued before invocations of `::await()` will be executed.
 ```
+ThreadPool pool(1);
+pool.enq_job(f); // execution of f queued (and started shortly after)
+pool.enq_job(g); // execution of g queued (waiting for f to finish to start executing)
 pool.await();
+// execution of both f and g finished
 pool.stop();
 ```
+Without `pool.await();` in the example above, `f` and `g` would probably not be executed at all, as the main calling thread is fast and stops the pool before calling the passed functions.
+
+## Stop
+You prevent unstarted jobs from the queue from being started with:
+```
+pool.stop();
+```
+Any running jobs will still be finished regularly.  
+
+Make sure to call `::await()` first, if you want all jobs to be executed.
+
+After stopping the `ThreadPool`, you can start it again, if you want unfinished jobs to be executed or if new jobs have been queued.
+
+## Additional info
+The destructor always calls `::stop()`.
+
+You cannot catch exceptions thrown by the passed lambda or `std::function` (yet).
--- include/ThreadPool.hpp
+++ include/ThreadPool.hpp
@@ -9,31 +9,160 @@
 #include <vector>
 #include <queue>
 #include <array>
+#include <atomic>
 
 class ThreadPool {
 public:
     ThreadPool() {}
-    ThreadPool(size_t num_threads) { start(num_threads); }
-    ThreadPool(const ThreadPool&) = default;
-    ThreadPool& operator=(const ThreadPool&) = default;
+    ThreadPool(size_t num_threads);
+    ThreadPool(const ThreadPool&) = delete;
+    ThreadPool& operator=(const ThreadPool&) = delete;
+    ThreadPool(ThreadPool&&) = delete;
+    ThreadPool& operator=(ThreadPool&&) = delete;
+    ~ThreadPool();
 
     void start(size_t num_threads);
     void enq_job(const std::function<void()>& job);
-    void stop();
+    size_t get_num_active_tasks();
     bool is_busy();
-
-    template<uint Intrvl = 1000>
-    void await() {
-    while (is_busy())
-        std::this_thread::sleep_for(std::chrono::milliseconds(Intrvl));
-}
+    void await();
+    void clear_jobs();
+    void stop();
 
 protected:
-    std::vector<std::thread> threads;
-    std::queue<std::function<void()>> jobs;
-    bool command_terminate = false;
-    std::mutex q_mtx;
-    std::condition_variable start_new_job_cond;
+    std::vector<std::thread> m_threads;
 
-    void ThreadLoop();
-};
+    std::queue<std::function<void()>> m_jobs;
+    bool m_command_stop = false;
+    std::mutex m_jobs_mtx;
+    std::condition_variable m_start_new_job;
+
+    std::atomic<size_t> m_num_active_tasks;
+    std::mutex m_busy_mtx;
+    std::condition_variable m_all_tasks_done;
+
+    void thread_loop();
+}; // class ThreadPool
+
+
+ThreadPool::ThreadPool(size_t num_threads)
+    : m_num_active_tasks(0)
+{
+    start(num_threads);
+}
+
+
+ThreadPool::~ThreadPool() {
+    stop();
+}
+
+
+void ThreadPool::start(size_t num_threads) {
+    {
+        std::unique_lock<std::mutex> jobs_lck(m_jobs_mtx);
+        m_command_stop = false;
+    }
+
+    size_t num_hardware_threads = (size_t)std::thread::hardware_concurrency();
+    m_threads.reserve(num_hardware_threads);
+
+    num_threads = std::min(num_hardware_threads, num_threads+m_threads.size());
+    for (size_t i=m_threads.size(); i<num_threads; ++i) {
+        m_threads.emplace_back(std::thread([this] {
+            thread_loop();
+        }));
+    }
+}
+
+
+void ThreadPool::thread_loop() {
+    while (true) {
+        std::function<void()> job;
+        
+        {
+            std::unique_lock<std::mutex> jobs_lck(m_jobs_mtx);
+            m_start_new_job.wait(jobs_lck, [this] {
+                return !m_jobs.empty() || m_command_stop;
+            });
+
+            if (m_command_stop) {
+                return;
+            }
+
+            job = m_jobs.front();
+            m_jobs.pop();
+        }
+
+        ++m_num_active_tasks;
+        job();
+        --m_num_active_tasks;
+
+        {
+            std::unique_lock<std::mutex> busy_lck(m_busy_mtx);
+            m_all_tasks_done.notify_all();
+        }
+    }
+}
+
+
+void ThreadPool::enq_job(const std::function<void()>& job) {
+    {
+        std::unique_lock<std::mutex> jobs_lck(m_jobs_mtx);
+        m_jobs.push(job);
+    }
+
+    m_start_new_job.notify_one();
+}
+
+
+size_t ThreadPool::get_num_active_tasks() {
+    return size_t(m_num_active_tasks);
+}
+
+
+bool ThreadPool::is_busy() {
+    std::unique_lock<std::mutex> jobs_lck(m_jobs_mtx);
+
+    return !m_jobs.empty() || bool(m_num_active_tasks);
+}
+
+
+void ThreadPool::await() {
+    {
+        std::unique_lock<std::mutex> jobs_lck(m_jobs_mtx);
+        if (m_command_stop) {
+            return;
+        }
+    }
+    std::unique_lock<std::mutex> busy_lck(m_busy_mtx);
+    m_all_tasks_done.wait(busy_lck, [this] {
+        return !is_busy();
+    });
+}
+
+
+void ThreadPool::clear_jobs() {
+    stop();
+
+    m_jobs = std::queue<std::function<void()>>();
+}
+
+
+void ThreadPool::stop() {
+    {
+        std::unique_lock<std::mutex> jobs_lck(m_jobs_mtx);
+        if (m_command_stop) {
+            return;
+        }
+        else {
+            m_command_stop = true;
+        }
+    }
+
+    m_start_new_job.notify_all();
+
+    for (std::thread& thread : m_threads)
+        thread.join();
+
+    m_threads.clear();
+}
